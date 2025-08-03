a8eeff02b851b1ff45cff33b19752f09c0f10e1e
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-03 16:05:21 -0400

    Add Celsius/Fahrenheit toggle to WeatherTemperature
    Implemented temperature unit switching in WeatherTemperature component. Users can now toggle between Fahrenheit and Celsius, with conversion handled in the component state.

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,14 +3,42 @@ import React, { useState } from "react";
 export default function WeatherTemperature(props) {
     const [unit, setUnit] = useState("fahrenheit");
 
-} 
 
-// if (unit === "farenheit") {
-//     return (
-//         <div className="WeatherTemperature">
-//             <span className="currentTemperature">
-//             {props.fahrenheit}
-//             <span className="unit">°F | <a href="/">°C</a></span>
-//             </span>
-//         </div>
-//     );
+    function convertToCelcius(event) {
+        event.preventDefault();
+        setUnit("celcius");
+    }
+
+    function convertToFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    if (unit === "fahrenheit") {
+        return (
+          <div className="WeatherTemperature">
+            <span className="currentTemperature">
+              {props.fahrenheit}{" "}
+              <span className="unit">
+                °F | <a href="/" onClick={convertToCelcius}>°C</a>
+              </span>{" "}
+            </span>
+          </div>
+        );
+    } else {
+        let celcius = Math.round((props.fahrenheit - 32) * 5 / 9);
+        return (
+          <div className="WeatherTemperature">
+            <span className="currentTemperature">
+              {celcius}{" "}
+              <span className="unit">
+                <a href="/" onClick={convertToFahrenheit}>
+                  °F </a>| °C
+              </span>{" "}
+            </span>
+          </div>
+        );
+    }
+
+    
+} 

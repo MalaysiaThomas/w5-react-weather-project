e801c0e5c4296d350fdc0e902a3aa60152ed12ad
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-03 16:22:57 -0400

    Add DailyForecast component to Weather
    Imported and rendered the DailyForecast component in the Weather component to display daily weather forecasts.

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Weather.css';
 import WeatherInfo from './WeatherInfo';
+import DailyForecast from './DailyForecast';
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({loaded: false});
@@ -66,6 +67,7 @@ export default function Weather(props) {
               </div>
             </form>
             <WeatherInfo data={weatherData}/>
+            <DailyForecast />
           </div>
         );
     } else {

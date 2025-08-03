25c99e36b564522e0952cf120b840d97b34b54ea
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-03 18:36:25 -0400

    Import useState to dynamically change forecast.

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,23 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './DailyForecast.css';
 import axios from 'axios';
 
 
-export default function DailyForecast(props) {
-  console.log(props)
-    function handleResponse(response) {
-       console.log(lat);
-       console.log(lon)
-     }
+export default function DailyForecast(props) { 
+  let [loaded, setLoaded] = useState(false)  
+  let [forecast, setForecast] = useState(null)  
 
-    const apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
-    let lat = props.coordinates.lat; 
-    let lon = props.coordinates.lon; 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-   
+  function handleResponse(response) {
+       setForecast(response.data.daily)
+       setLoaded(true)
+  }
 
-    axios.get(apiUrl).then(handleResponse)
-
+  if (loaded) {
+    console.log(forecast)
     return (
       <div className="DailyForecast">
         <div className="row">
@@ -39,5 +35,16 @@ export default function DailyForecast(props) {
           
         </div>
       </div>
-    );
+    )
+  } else {
+        let apiKey = "34a0b3608792f91t1oc6463e450b7ab0";
+        let lat = props.coordinates.lat; 
+        let lon = props.coordinates.lon; 
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`
+        
+
+        axios.get(apiUrl).then(handleResponse)
+
+        return null;
+  }
 }

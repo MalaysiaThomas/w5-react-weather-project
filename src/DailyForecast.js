6b1ea6ba1baeceecbc178566ccf78d2806d363a6
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-03 19:04:57 -0400

    refactor to add forecast into Forecast Date component for improved readability

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './DailyForecast.css';
 import axios from 'axios';
-
+import ForecastDate from "./ForecastDate"
 
 export default function DailyForecast(props) { 
   let [loaded, setLoaded] = useState(false)  
@@ -18,19 +18,7 @@ export default function DailyForecast(props) {
       <div className="DailyForecast">
         <div className="row">
           <div className="col">
-            <div className="DailyForecast-day">Mon</div>
-
-            <div className="DailyForecast-icon">
-              <img
-                src="https://openweathermap.org/img/wn/01d@2x.png"
-                alt="Sunny"
-              />
-            </div>
-
-            <div className="DaiyForecast-temperatures">
-              <span className="DailyForecast-temperature-max">72°</span>
-              <span className="DailyForecast-temperature-min">55°</span>
-            </div>
+            <ForecastDate data={forecast[0]}/>
           </div>
           
         </div>
